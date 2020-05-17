afae983479d8873adb73ab2626d8a5538ac2524d
Alexander Watzinger <alexander.watzinger@craws.net>
2020-05-17 14:55:49 +0200

    Update changelog

--- openatlas_frontend/util/changelog.py
+++ openatlas_frontend/util/changelog.py
@@ -1,7 +1,10 @@
 class Changelog:
     versions = {
         '0.2.0': ['TBA', {
-            'feature': {},
+            'feature': {
+                '1243': 'Testing framework',
+                '1246': 'Changelog',
+            },
             'fix': {}}],
         '0.1.0': ['2020-05-15', {
             'feature': {
