2ea83de80a9d254c55905579c8185854fe73ccb3
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2020-07-22 21:09:14 +0200

    basic API interaction estabblished

---
+++ pages/actors.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  mounted() {
+    console.log(this.$api.Users.retrieveQuery({ 'classes[]': ['E21', 'E74', 'E40'], limit: 10 }));
+  },
+};
+</script>
--- plugins/api.js
+++ plugins/api.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import Swagger from 'swagger-client'
-import APISpec from '../assets/swagger.json'
+import Vue from 'vue';
+import Swagger from 'swagger-client';
+import APISpec from '../assets/swagger.json';
 
 const APIClient = {
-  install (Vue) {
+  install(Vue) {
     Swagger({ spec: APISpec }).then((client) => {
-      Vue.prototype.$client = client
-      Vue.prototype.$api = client.apis
+      Vue.prototype.$client = client;
+      Vue.prototype.$api = client.apis;
     }, (error) => {
-      console.error('failed to load api spec: %o', error)
-    })
-  }
-}
+      console.error('failed to load api spec: %o', error);
+    });
+  },
+};
 
-Vue.use(APIClient)
+Vue.use(APIClient);
