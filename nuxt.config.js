0b3291845cc5f0787b37744c765ddf5455cd452e
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2020-10-14 20:44:37 +0200

    dist dir added

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'static',
+  target: 'server',
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
@@ -104,7 +104,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
-    analyze: true,
+    analyze: false,
     extractCSS: {
       ignoreOrder: true,
     },
