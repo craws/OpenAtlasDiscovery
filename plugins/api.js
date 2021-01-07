23efafe87e6dc59fbea14823aaf914272c97d1b4
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2021-01-07 22:13:13 +0100

    new api

--- plugins/api.js
+++ plugins/api.js
@@ -7,6 +7,7 @@ const APIClient = {
     Swagger({ spec: APISpec }).then((client) => {
       Vue.prototype.$client = client;
       Vue.prototype.$api = client.apis;
+      console.log(Vue.prototype.$api);
     }, (error) => {
       console.error('failed to load api spec: %o', error);
     });
