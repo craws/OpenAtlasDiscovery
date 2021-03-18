05cc0c383dad2abd3e39ed817f4da6578ff72185
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2021-03-19 00:01:58 +0100

    relations in table

--- pages/single/_id.vue
+++ pages/single/_id.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
-    <v-layout v-if="!loading" class="mt-3">
-      <v-row>
-        <v-col
-          xs="6"
-        >
-          <v-row no-gutters wrap>
+    <v-layout v-if="!loading" class="ma-1">
+      <v-row no-gutters>
+        <v-col xs="6">
+          <v-row no-gutters class="pr-1">
             <v-col cols="12" xs="12">
               <v-card
                 class="pa-4"
@@ -37,37 +35,82 @@
                 </v-row>
               </v-card>
             </v-col>
-            <v-col
-              xs="12"
-            >
+            <v-col cols="12" xs="12" v-if="item.features[0].description">
               <v-card
                 class="pa-4"
                 outlined
                 tile
               >
-                <div v-if="item.features[0].description" class="text-body-2">
+                <v-row align="center" class="pl-4 pb-4" >
+                  <v-icon class="pr-2">mdi-comment</v-icon>
+                  <div class="text-overline">Description</div>
+                </v-row>
+                <div class="text-body-2">
                   {{
                     item.features[0].description[0].value
                   }}
                 </div>
               </v-card>
             </v-col>
+            <v-col cols="12" xs="12">
+              <v-card
+                class="pa-4"
+                outlined
+                tile
+              >
+                <v-row align="center" class="pl-4" >
+                  <v-icon class="pr-2">mdi-comment</v-icon>
+                  <div class="text-overline">Relations</div>
+                </v-row>
+                <v-row class="pl-4">
+                  <v-data-table
+                    :headers="$store.state.app.tableheaders.relations"
+                    :items="item.features[0].relations"
+                    :expanded.sync="expanded"
+                    item-key="label"
+                    group-by="relationType"
+                    :items-per-page="-1"
+                    class="elevation-0"
+                    style="width:100%"
+                  >
+                    <template v-slot:group.header="{ group, headers, toggle, isOpen }">
+                      <td :colspan="headers.length">
+                        <v-btn @click="toggle" small icon :ref="group" :data-open="isOpen">
+                          <v-icon v-if="isOpen">mdi-chevron-up</v-icon>
+                          <v-icon v-else>mdi-chevron-down</v-icon>
+                        </v-btn>
+                        {{ group }}
+                      </td>
+                    </template>
+                    <template v-slot:item.label="{ item }">
+                      <nuxt-link :to="`/single/${item.relationTo.split('/').splice(-1)[0]}`">
+                        {{ item.label }}
+                      </nuxt-link>
+                    </template>
+                  </v-data-table>
+                </v-row>
+              </v-card>
+            </v-col>
           </v-row>
         </v-col>
-        <v-col
-          xs="6"
-        />
+        <v-col xs="6" >
+          <div style="height: calc(100vh - 72px)">
+            <qmap v-if="!this.loading" :geojsonitems="geojsonitems"/>
+          </div>
+        </v-col>
       </v-row>
     </v-layout>
   </div>
 </template>
 
 <script>
-
 import { mapGetters } from 'vuex';
+import qmap from '~/components/map.vue';
 
 export default {
-  components: {},
+  components: {
+    qmap,
+  },
   async fetch() {
     this.loading = true;
     // eslint-disable-next-line no-underscore-dangle
@@ -76,13 +119,42 @@ export default {
     });
     // eslint-disable-next-line prefer-destructuring
     this.item = p.body;
+    // await this.fetchRelated();
     this.loading = false;
   },
   data() {
     return {
       item: {},
+      related: [],
+      expanded: [],
       loading: false,
     };
+  },
+  methods: {
+    async fetchRelated() {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const id of this.item.features[0].relations) {
+        // eslint-disable-next-line no-await-in-loop,no-underscore-dangle
+        const ri = await this.$api.Entities.get_api_0_2_entity__id__({
+          id_: id.relationTo.split('/').splice(-1, 1),
+        });
+        this.related.push(ri.body);
+      }
+    },
+    closeAll() {
+      Object.keys(this.$refs).forEach((k) => {
+        if (this.$refs[k] && this.$refs[k].$attrs['data-open']) {
+          this.$refs[k].$el.click()
+        }
+      })
+    },
+    openAll() {
+      Object.keys(this.$refs).forEach((k) => {
+        if (this.$refs[k] && !this.$refs[k].$attrs['data-open']) {
+          this.$refs[k].$el.click()
+        }
+      })
+    }
   },
   computed: {
     ...mapGetters('app', [
@@ -91,6 +163,13 @@ export default {
       'getCRMClassBySystemClass',
       'getSortColumnByPath',
     ]),
+    geojsonitems() {
+      return [this.item, ...this.related];
+    },
+    relationTypes() {
+      if (Array.isArray(this.item.features[0].relations)) return [...new Set(this.item.features[0].relations.map((item) => item.relationType))];
+      return [];
+    },
   },
   watch: {
     '$route.params': {
@@ -100,6 +179,9 @@ export default {
       immediate: true,
       deep: true,
     },
+  },
+  created() {
+    this.closeAll();
   },
 };
 </script>
