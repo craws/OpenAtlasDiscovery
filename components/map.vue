2579c769ec939f554a37c0abb9eb764190615f1b
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2021-12-07 19:43:56 +0100

    map reorganized

--- components/map.vue
+++ components/map.vue
@@ -4,11 +4,14 @@
       <l-map ref="map" :zoom="13" :center="center" :options="options">
         <l-tile-layer :url="url" />
         <l-feature-group ref="features">
-          <l-geo-json
-            v-for="item in geojsonitems"
+          <l-marker
+            v-for="item in pointsFromGeoJSON"
             :key="item.features[0]['@id']"
-            :geojson="item"
-          />
+            :lat-lng="item.features[0].geometry.coordinates.slice().reverse()"/>
+          <l-popup
+            v-for="item in pointsFromGeoJSON"
+            :key="item.features[0]['@id']"
+          >{{ item.features[0].geometry.title }}</l-popup>
         </l-feature-group>
       </l-map>
     </div>
@@ -54,6 +57,14 @@ export default {
       immediate: true,
     },
   },
+  computed: {
+    pointsFromGeoJSON() {
+      return this.geojsonitems.filter((f) => {
+        if (f.features[0].geometry && f.features[0].geometry.type === 'Point') return true;
+        return false;
+      });
+    },
+  },
   methods: {
     delayedHandler() {
       this.$nextTick(() => {
@@ -66,6 +77,9 @@ export default {
       const bounds = this.$refs.features.mapObject.getBounds();
       // eslint-disable-next-line no-underscore-dangle
       if (bounds._northEast) this.$refs.map.mapObject.fitBounds(bounds);
+    },
+    reverseCoordinates(c) {
+      return c.reverse();
     },
   },
 };
