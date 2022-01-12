ade8fc9e4f3652971b2aacea951c748290591f53
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2022-01-12 10:44:47 +0100

    cummulative updates connec

---
+++ pages/typefilters/_q.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <list :filter="query" />
+  </div>
+</template>
+
+<script>
+import list from '~/components/lists/list.vue';
+import queries from '~/mixins/queries';
+
+export default {
+  components: {
+    list,
+  },
+  mixins: [queries],
+  data() {
+    return {
+      query: {},
+      view: 'list',
+    };
+  },
+  watch: {
+    '$route.params': {
+      handler(s) { this.parseQuery(s.q); },
+      immediate: true,
+      deep: true,
+    },
+  },
+  methods: {
+  },
+};
+</script>
