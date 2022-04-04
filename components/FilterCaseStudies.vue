b0d6958bb205903e2762033087f593db747b6d27
Andi <andreas@olschnoegger.at>
2022-04-04 10:48:40 +0200

    extract Filter for CaseStudies

---
+++ components/FilterCaseStudies.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-row no-gutters>
+    <v-col v-for="item in caseStudyCheckboxes" :key="item.id" cols="12">
+      <div class="d-flex justify-space-between align-center">
+        <v-checkbox v-model="item.selected" :label="item.name" :color="item.color"/>
+        <v-btn
+          icon
+          class="expand-button"
+          :class="{ clicked: item.expanded }"
+          @click="item.expanded = !item.expanded"
+        >
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </div>
+
+      <v-expand-transition>
+        <v-row v-if="item.expanded" no-gutters class="ml-5">
+          <v-col v-for="subitem in item.subtypes" :key="subitem.id" cols="12">
+            <v-checkbox
+              v-model="subitem.selected"
+              class="mt-0"
+              :color="item.color"
+              :label="subitem.name"
+            />
+          </v-col>
+        </v-row>
+      </v-expand-transition>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'FilterCaseStudies',
+  data() {
+    return {
+      caseStudyCheckboxes: [],
+    };
+  },
+  computed: {
+    ...mapGetters('data', ['getCaseStudies'])
+  },
+  mounted() {
+    this.initCaseStudies();
+  },
+  methods: {
+    ...mapActions('data', ['loadTypeTree']),
+    initCaseStudies() {
+      this.caseStudyCheckboxes = this.getCaseStudies.map((x) => ({
+        ...x,
+        subtypes: x.subtypes.map((y) => ({
+          ...y,
+          selected: true,
+        })),
+        selected: true,
+        expanded: false,
+      }));
+    }
+  },
+  watch: {
+    getCaseStudies() {
+      this.initCaseStudies();
+
+    },
+    caseStudyCheckboxes: {
+      handler() {
+        const value = this.caseStudyCheckboxes
+          .flatMap((x) => [x.selected && x.id, ...x.subtypes.filter((y) => y.selected)
+            .map((y) => y.id)]);
+        this.$emit('input', value);
+      },
+      deep: true,
+      immediate: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.expand-button {
+  transition: all ease-in-out 100ms;
+}
+
+.expand-button.clicked {
+  transform: rotate(90deg);
+}
+</style>
